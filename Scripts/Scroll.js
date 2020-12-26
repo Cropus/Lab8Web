26da873aaa0e7851c5d8dc724a73c9fee23060d5
Cropus <mr.flashmob@bk.ru>
2020-12-26 05:48:24 +0300

    Initial commit

---
+++ Scripts/Scroll.js
@@ -0,0 +1,18 @@
+mybutton = document.getElementById("scrollButton");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
